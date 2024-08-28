f75482d32533e85129cf9d3f271415474ee10846
Tiago Cunha <tiagocunha.rsilva@gmail.com>
2024-08-28 13:37:43 -0300

    Tratamento de exceções para caso o usuário mova a peça para um lugar já ocupado, ou para um lugar que não exista

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -4,7 +4,12 @@ public class Board {
 	private int column;
 	private int rows;
 	private Piece [][] piece;
+	
 	public Board(int column, int rows) {
+		if (rows < 1 || column < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
+		
 		this.column = column;
 		this.rows = rows;
 		piece = new Piece[rows][column];
@@ -13,27 +18,49 @@ public class Board {
 	public int getColumn() {
 		return column;
 	}
-	public void setColumn(int column) {
-		this.column = column;
-	}
+	
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 	
 	public Piece piece (int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return piece[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return piece[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(therIsAPiece(position)) {
+		throw new BoardException("There is already a piece in this place.");
+		}
+		
+		
 		this.piece [position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows  && column >= 0 && column < this.column;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean therIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		return piece(position) != null;
 	}
 
 }
