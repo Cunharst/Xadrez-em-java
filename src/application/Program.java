59f28c6b938a9bb000b32e4ba057e4647259bc46
Tiago Cunha <tiagocunha.rsilva@gmail.com>
2024-08-28 23:02:55 -0300

    Movimentos podem ser feitos, métodos feitos para remover e mover peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPosition;
+import chess.chessPiece;
 
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
 		ChessMatch cm = new ChessMatch();
+		
+		while(true) {
 		UI.printBoard(cm.getPiecess());
+		System.out.println();
+		System.out.println("Source");
+		ChessPosition source = UI.readChessPosition(entrada);
 		
+		System.out.println("Target");
+		ChessPosition target = UI.readChessPosition(entrada);
+		chessPiece capturedPiece = cm.performChessMove(source,target);
 		
-
+		}
+		
+	
 	}
 
 }
